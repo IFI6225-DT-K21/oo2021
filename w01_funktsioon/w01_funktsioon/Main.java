9fc5bfdc92dc70930f33c916cd743a62ca59b7bd
Karl Pihlakas <karlpihlakas@users.noreply.github.com>
2021-02-09 14:31:24 +0200

    W02classwork

--- w01_funktsioon/w01_funktsioon/Main.java
+++ w01_funktsioon/w01_funktsioon/Main.java
@@ -1,31 +1,24 @@
-package w01_funktsioon.w01_funktsioon;
+
 public class Main {
     public static void main(String [] args ) {
 
-       float fileSize = 100;
-       float internetSpeed = 2.048f;
-       float overHeadInput = 13;
-       float overHead = overHeadInput / 100;
-
-       float fullTime = ((fileSize / internetSpeed) * overHead);
-       float timeInMinutes = fullTime / 60;
-    
-       
-      
-       System.out.println(Math.round(fullTime));
-       System.out.println(timeInMinutes);
+       calc();
      
         
     }
 
-  /*   public static int calc(int fileSize, int internetSpeed, int overHead) {
-        
-        int fullTime = ((fileSize / internetSpeed) * overHead) ;
-        //int resultInMinutes = fullTime * 60 ;
-        return fullTime; 
-        
-    } 
-    
-    */
+
+    public static void calc() {
+
+        float fileSize = 1000;
+        float internetSpeed = 2.048f;
+        float overHeadInput = 13;
+        float overHead = overHeadInput / 100;
+
+       float fullTime = ((fileSize / internetSpeed) * overHead);
+       float timeInMinutes = fullTime / 60;
+       System.out.println(Math.round(fullTime));
+       System.out.println(timeInMinutes);
+    }
 }
 
