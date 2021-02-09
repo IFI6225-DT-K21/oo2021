20c3c361152aa4084fe61bd2aee1611be3167654
Karl Pihlakas <karlpihlakas@users.noreply.github.com>
2021-02-09 14:29:57 +0200

    Week02

---
+++ w02_klass/tunnitoo/Proov1.java
@@ -0,0 +1,15 @@
+
+public class Proov1 {
+    public static void main(String[] args) {
+        Isikukood Karl = new Isikukood("39702150844");
+
+        System.out.println(Karl.kuuNumber());
+        
+        System.out.println(Karl.kuuPaev());
+
+        System.out.println(Karl.sugu());
+        
+        System.out.println("ns");
+        }
+        
+}
